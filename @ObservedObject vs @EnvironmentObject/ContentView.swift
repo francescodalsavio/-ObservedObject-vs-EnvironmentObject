85c32ee4e239f02abc43e71c5aa784f75958beae
Francesco Dal Savio <dalsaviof@gmail.com>
2021-01-06 13:19:30 +0100

    Initial Commit

---
+++ @ObservedObject vs @EnvironmentObject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  @ObservedObject vs @EnvironmentObject
+//
+//  Created by Francesco Dal Savio on 06/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ @ObservedObject vs @EnvironmentObject/_ObservedObject_vs__EnvironmentObjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  _ObservedObject_vs__EnvironmentObjectApp.swift
+//  @ObservedObject vs @EnvironmentObject
+//
+//  Created by Francesco Dal Savio on 06/01/21.
+//
+
+import SwiftUI
+
+@main
+struct _ObservedObject_vs__EnvironmentObjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
